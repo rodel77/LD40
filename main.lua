e6ac1868573aed1e76f2359f3bd291b601dc3731
rodel77 <rodel1407@hotmail.com>
2017-12-02 15:39:18 -0600

    Some other basic stuff

--- main.lua
+++ main.lua
@@ -1,18 +1,36 @@
 local CScreen = require "cscreen";
 
+-- Dungeon Crawler Turn Based vs AI, turns increment to player and AI each turn
+-- And where is the theme?: The more turns you have, the more damage AI can make you
+
 function love.load()
     -- Screen setup
-    CScreen.init(800, 700, true);
+    -- The extra 100 pixels are for GUI & stuff
+    CScreen.init(900, 700, true);
     CScreen.setColor(255, 0, 0);
 
     love.graphics.setDefaultFilter("nearest", "nearest");
-    img = love.graphics.newImage("ld40.png");
+    loadAssets();
+end
+
+function loadAssets()
+    atlas = love.graphics.newImage("assets/atlas.png");
+    grass_tile = love.graphics.newQuad(0, 0, 25, 25, atlas:getDimensions());
+
 end
 
 function love.draw()
     CScreen.apply();
-    love.graphics.draw(img, 0, 0);
+    for i=0,6*100,100 do
+        for j=0,6*100,100 do
+            love.graphics.draw(atlas, grass_tile, i, j, 0, 4, 4);
+        end
+    end
+    -- #DryRules...
     CScreen.cease();
+end
+function love.update()
+
 end
 
 function love.resize(w, h)
